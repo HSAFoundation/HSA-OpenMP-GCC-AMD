233f4c0ff7020d0573aa032c619893e58cedc403
gregrodgers <grodgers@purdue.edu>
2016-02-29 15:25:04 -0600

    Update to svn version 233469

--- usr/local/hsagccver/hsagcc.229090/lib/gcc/x86_64-pc-linux-gnu/6.0.0-hsa-rev-229090/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../src/configure --enable-offload-targets=hsa --with-hsa-runtime=/opt/hsa --disable-bootstrap --enable-languages=c,c++,fortran --prefix=/usr/local/hsagccver/hsagcc.229090 --disable-multilib";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
