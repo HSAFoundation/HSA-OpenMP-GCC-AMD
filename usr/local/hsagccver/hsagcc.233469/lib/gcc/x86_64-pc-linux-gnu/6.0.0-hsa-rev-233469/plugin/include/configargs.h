c284ff4f877c4a51a2b0b12b1823802b5c921e09
Greg Rodgers <grodgers@purdue.edu>
2016-05-11 14:59:32 -0500

    moving to ROCM package management for hsa gcc6

--- usr/local/hsagccver/hsagcc.233469/lib/gcc/x86_64-pc-linux-gnu/6.0.0-hsa-rev-233469/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "../src.next/configure --enable-offload-targets=hsa --with-hsa-runtime=/opt/hsa --disable-bootstrap --enable-languages=c,c++ --prefix=/usr/local/hsagccver/hsagcc.233469 --without-isl --disable-multilib";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
